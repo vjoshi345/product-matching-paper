3c914eaa24c384084779f287fb9b579c0fc68367
Varun Joshi <vjoshi345@gmail.com>
2024-07-06 14:48:23 -0400

    use price in matching

--- fuzzy_matching.py
+++ fuzzy_matching.py
@@ -21,6 +21,7 @@ data_list.pop(0)
 dfA = pd.DataFrame(data_list, columns=cols)
 dfA.set_index(keys=['id'], drop=True, inplace=True, verify_integrity=True)
 dfA['desc_100'] = [desc[:100] for desc in dfA['description']]
+dfA['price'] = dfA['price'].astype(float)
 print(dfA.shape)
 print(dfA.dtypes)
 
@@ -39,6 +40,7 @@ data_list.pop(0)
 dfB = pd.DataFrame(data_list, columns=cols)
 dfB.set_index(keys=['id'], drop=True, inplace=True, verify_integrity=True)
 dfB['desc_100'] = [desc[:100] for desc in dfB['description']]
+dfB['price'] = dfB['price'].astype(float)
 print(dfB.shape)
 print(dfB.dtypes)
 
@@ -76,12 +78,14 @@ compare_cl.string("title", "name", method='jarowinkler', threshold=0.95, label="
 # compare_cl.string("description", "description", method='jarowinkler', threshold=0.95, label="description")
 compare_cl.string("desc_100", "desc_100", method='jarowinkler', threshold=0.95, label="desc_100")
 compare_cl.string("manufacturer", "manufacturer", method='jarowinkler', threshold=0.95, label="manufacturer")
-compare_cl.string("price", "price", method='jarowinkler', threshold=0.95, label="price")
+compare_cl.numeric("price", "price", method='linear', label="price")
 
 features = compare_cl.compute(candidate_links, dfA, dfB)
 
 print('\nDistribution of records matching on all columns')
-print(features.sum(axis=1).value_counts().sort_index(ascending=False))
+features['price_comp'] = features['price'] >= 0.95
+features['sum_all'] = features['title'] + features['desc_100'] + features['manufacturer'] + features['price_comp']
+print(features['sum_all'].value_counts(ascending=False))
 # features['sum_title_desc_manu'] = features['title'] + features['description'] + features['manufacturer']
 features['sum_title_desc_manu'] = features['title'] + features['desc_100'] + features['manufacturer']
 print(features.shape)
@@ -91,12 +95,31 @@ print(features.head())
 # Classification step
 print('*'*80)
 print('Classification and evaluation for fuzzy matching...')
-for match_col in [1, 3]:
+for match_col in [1, 2, 3]:
     if match_col == 1:
         print(f'Link if one of title, description, or manufacturer matches:')
+    elif match_col == 2:
+        print(f'Link if two or more of title, description, or manufacturer matches:')
     elif match_col == 3:
         print(f'Link if all of title, description, and manufacturer match:')
     matches = features[features['sum_title_desc_manu'] >= match_col]
+    print(matches.shape)
+    print(matches.dtypes)
+    print(matches.head())
+
+    print('Reduction ratio:', recordlinkage.reduction_ratio(matches.index, dfA, dfB))
+    print('Precision:', recordlinkage.precision(links_true=df_link, links_pred=matches.index))
+    print('Recall:', recordlinkage.recall(links_true=df_link, links_pred=matches.index))
+    print('F1 score:', recordlinkage.fscore(links_true=df_link, links_pred=matches.index))
+    print('Confusion matrix:', recordlinkage.confusion_matrix(links_true=df_link, links_pred=matches.index,
+                                                              total=len(dfA)*len(dfB)))
+    print('*'*40)
+
+print('*'*80)
+print('Classification and evaluation for fuzzy matching with price...')
+for match_col in [1, 2, 3, 4]:
+    print(f'Link if {match_col} or more of the columns match...')
+    matches = features[features['sum_all'] >= match_col]
     print(matches.shape)
     print(matches.dtypes)
     print(matches.head())
